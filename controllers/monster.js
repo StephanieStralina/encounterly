8341c2fb33f819327fffaeec627b9e41aeeefaa6
StephanieStralina <StephanieDRichter@gmail.com>
2024-12-27 12:19:50 -0600

    Add POST route and create functionality for Monsters

--- controllers/monster.js
+++ controllers/monster.js
@@ -4,8 +4,9 @@ const Monster = require('../models/monster');
 const User = require('../models/user');
 
 //GET /monsters (index functionality)
-router.get('/', (req, res) => {
-    res.render('monsters/index.ejs');
+router.get('/', async (req, res) => {
+    const monsters = await Monster.find({});
+    res.render('monsters/index.ejs', { monsters });
 });
 
 //GET /monsters/new (new functionality)
@@ -13,7 +14,17 @@ router.get('/new', (req, res) => {
     res.render('monsters/new.ejs');
 });
 
-
+//POST /monsters (create functionality)
+router.post('/', async (req, res) => {
+    try {
+        req.body.user = req.user._id;
+        await Monster.create(req.body);
+        res.redirect('/monsters', { Monster });
+    } catch (e) {
+        console.log(e);
+        res.redirect('/monsters'); //TODO add error page
+    }
+});
 
 
 module.exports = router;
--- public/js/form-handler.js
+++ public/js/form-handler.js
@@ -1,9 +1,29 @@
 let index = 0;
 
 function addAbility() { 
-    index++;
+    index++; 
     const container = document.getElementById('abilities-container'); 
-    const nameField = `<label for="abilities-name">Name:</label> <input type="text" name="details.abilities[${index}].name" id="abilities-name" />`; 
-    const descriptionField = `<label for="abilities-description">Description:</label> <input type="text" name="details.abilities[${index}].description" id="abilities-description" />`; 
-    container.innerHTML += nameField + descriptionField; 
+
+    const nameLabel = document.createElement('label'); 
+    nameLabel.setAttribute('for', `abilities-name-${index}`); 
+    nameLabel.textContent = 'Name:'; 
+
+    const nameInput = document.createElement('input'); 
+    nameInput.setAttribute('type', 'text'); 
+    nameInput.setAttribute('name', `details.abilities[${index}].name`); 
+    nameInput.setAttribute('id', `abilities-name-${index}`); 
+    
+    const descriptionLabel = document.createElement('label'); 
+    descriptionLabel.setAttribute('for', `abilities-description-${index}`); 
+    descriptionLabel.textContent = 'Description:'; 
+    
+    const descriptionInput = document.createElement('input'); 
+    descriptionInput.setAttribute('type', 'text'); 
+    descriptionInput.setAttribute('name', `details.abilities[${index}].description`); 
+    descriptionInput.setAttribute('id', `abilities-description-${index}`); 
+    
+    container.appendChild(nameLabel); 
+    container.appendChild(nameInput); 
+    container.appendChild(descriptionLabel); 
+    container.appendChild(descriptionInput);
 }
