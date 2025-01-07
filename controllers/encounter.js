292ca22d7229a70f949ff8d5cd7a8217b3087c8e
StephanieStralina <StephanieDRichter@gmail.com>
2025-01-07 06:29:34 -0600

    Add new encounter form functionality

--- controllers/encounter.js
+++ controllers/encounter.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const Encounter = require('../models/encounter');
+const Monster = require('../models/monster');
 const User = require('../models/user');
 
 //GET /encounters (index functionality)
 router.get('/', async (req, res) => {
     const encounters = await Encounter.find({ user: req.user._id} );
-    res.render('encounters/index.ejs', { encounters });
+    res.render('encounters/index.ejs', { encounters});
 });
+
+//GET /encounters/new (new functionality)
+router.get('/new', async(req,res) => {
+    try {
+        const players = req.user.players;
+        const monsters = await Monster.find({ user: req.user._id});
+        res.render('encounters/new.ejs', { players, monsters });
+    } catch (e) {
+        console.log(e);
+        res.redirect('/encounters');
+    }
+});
+
+
+
+
+
+
+
 
 
 module.exports = router;
